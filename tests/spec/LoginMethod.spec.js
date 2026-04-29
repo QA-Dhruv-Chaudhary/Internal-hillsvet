0214e0e93da1b1873999985d3fe59f4c12bc3b13
dhuv patel <dhruv.patel@qable.io>
2026-04-29 17:04:54 +0530

    Framework ready for use

--- tests/spec/LoginMethod.spec.js
+++ tests/spec/LoginMethod.spec.js
@@ -2,10 +2,9 @@ const { test } = require('@playwright/test');
 const LoginMethod = require('../page/LoginMethod');
 
 test.describe('Login Tests', () => {
-  test('Test case 1: Positive LogIn test', async ({ page }) => {
+  test('Test case 1: Demo Login', async ({ page }) => {
     const loginPage = new LoginMethod(page);
     await loginPage.openPage();
     await loginPage.demoLogin();
   });
-
 });
